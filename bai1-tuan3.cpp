5f941d89ce8d75df8a0957e241a094b6ba8c449b
58131283 <46161224+58131283@users.noreply.github.com>
2019-01-08 09:16:33 +0700

    Add files via upload

---
+++ bai1-tuan3.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void Songaunhien(int *A, int n, int m) {
+    srand(time(NULL));
+    int randNum = 0;
+    for (int i = 0; i < n; i++) {
+        randNum = 1+rand() % m;
+        A[i] = randNum;
+    }
+}
+
+int main() {
+    int *A;
+    int n ; /* so luong phan tu  */
+    int m ; /* Moi phan tu có giá tri ngau nhiên nam trong khoang tu 0 den k */
+
+	printf("nhap m:");
+	scanf("%3d",&m);
+	do{
+		printf("\nnhap n:");
+		scanf("%3d",&n);
+	}
+	while(n>m);
+    A = new int[n]; // Cap phát bo nho cho mang A
+
+    Songaunhien(A, n, m);
+
+    /* Xu?t m?ng */
+    for (int i = 0; i < n; i++) {
+        printf("%4d", A[i]);
+    }
+    printf("\n");
+
+    delete[]A; // xoa vùng nho sau khi su dung
+    return 0;
+}
